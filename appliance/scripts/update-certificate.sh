20cf52c95715616a8fe03006fa1520849fe14efe
Ben Patterson <ben@robotjamie.com>
2019-05-17 12:06:43 +1000

    Finish cert renewal updates

--- appliance/scripts/update-certificate.sh
+++ appliance/scripts/update-certificate.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-KEYDIR=/var/www/certificates
-LEKEYDIR=/etc/letsencrypt/live/memory.grid.robotjamie.com
 certbot renew
-cat $LEKEYDIR/privkey.pem $LEKEYDIR/cert.pem > $KEYDIR/merged.pem
-cp $LEKEYDIR/chain.pem $KEYDIR/chain.pem
-chown -R :www-data $KEYDIR
-chmod -R g+x $KEYDIR
-
-/etc/init.d/lighttpd restart
